aeb27c807c0be0b94d8880d54415c590915add60
gilbert-ku <gilbert45ku@gmail.com>
2023-08-30 00:04:30 +0300

    57% done

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+-- Inserting bear records
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+  ('Mr. Chocolate', 5, 'M', 'Brown', 'Playful', 1),
+  ('Rowdy', 3, 'M', 'Black', 'Energetic', 1),
+  ('Tabitha', 6, 'F', 'Gray', 'Calm', 1),
+  ('Sergeant Brown', 8, 'M', 'Brown', 'Brave', 1),
+  ('Melissa', 4, 'F', 'Golden', 'Friendly', 1),
+  ('Grinch', 10, 'M', 'Green', 'Mischievous', 1),
+  ('Wendy', 7, 'F', 'White', 'Gentle', 1),
+  ('unnamed', NULL, NULL, NULL, NULL, NULL);
